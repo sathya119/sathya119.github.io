1832e04127630dfec2174d6c1d3d9618a0a5be1f
Sai Sanjay S <saisanjay@saibrains.com>
2025-02-21 14:04:49 +0000

    change 2

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,7 +13,7 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>by Hursh Gupta.</p>
+        <p>Be Kind, Be Yourself.</p>
         {/* <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
