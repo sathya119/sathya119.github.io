bd56eb6ea984c87b56cbb039ddf33409fabb7ae9
Sai Sanjay S <saisanjay@saibrains.com>
2025-02-21 14:13:21 +0000

    chng

--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -24,17 +24,17 @@ function boolToStringBool(b: boolean): string {
 export default ((opts: Options) => {
   const Comments: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     return (
-      <div
-        class={classNames(displayClass, "giscus")}
-        data-repo={opts.options.repo}
-        data-repo-id={opts.options.repoId}
-        data-category={opts.options.category}
-        data-category-id={opts.options.categoryId}
-        data-mapping={opts.options.mapping ?? "url"}
-        data-strict={boolToStringBool(opts.options.strict ?? true)}
-        data-reactions-enabled={boolToStringBool(opts.options.reactionsEnabled ?? true)}
-        data-input-position={opts.options.inputPosition ?? "bottom"}
-      ></div>
+     // <div
+     //   class={classNames(displayClass, "giscus")}
+     //   data-repo={opts.options.repo}
+     //   data-repo-id={opts.options.repoId}
+     //   data-category={opts.options.category}
+     //   data-category-id={opts.options.categoryId}
+     //   data-mapping={opts.options.mapping ?? "url"}
+     //   data-strict={boolToStringBool(opts.options.strict ?? true)}
+     //   data-reactions-enabled={boolToStringBool(opts.options.reactionsEnabled ?? true)}
+     //   data-input-position={opts.options.inputPosition ?? "bottom"}
+     // ></div>
     )
   }
 
