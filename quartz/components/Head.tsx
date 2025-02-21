08b4d9ed3d73d1f82f8fe19afd78ab977fc99096
Sai Sanjay S <saisanjay@saibrains.com>
2025-02-21 15:38:28 +0000

    preview

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -32,7 +32,7 @@ export default (() => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
+        {cfg.baseUrl && <meta property="card.png" content={ogImagePath} />}
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="675" />
         <link rel="icon" href={iconPath} />
